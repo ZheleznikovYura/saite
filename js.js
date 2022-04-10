7f6dde7c80c19899dd07bc35cf6ec1b4fcf3580d
ZheleznikovYura <103385065+ZheleznikovYura@users.noreply.github.com>
2022-04-10 23:44:58 +0400

    new

---
+++ js.js
@@ -0,0 +1,59 @@
+const headphones = [
+   {
+      img: "img/image_1.png",
+      title: "Apple BYZ S852I",
+      price: 2927,
+      rate: 4.7,
+   },
+   {
+      img: "img/image_2.png",
+      title: "Apple EarPods",
+      price: 2327,
+      rate: 4.5,
+   },
+   {
+      img: "img/image_3.png",
+      title: "Apple EarPods",
+      price: 2327,
+      rate: 4.5,
+   },
+   {
+      img: "img/image_1.png",
+      title: "Apple BYZ S852I",
+      price: 2927,
+      rate: 4.7,
+   },
+   {
+      img: "img/image_2.png",
+      title: "Apple EarPods",
+      price: 2327,
+      rate: 4.5,
+   },
+   {
+      img: "img/image_3.png",
+      title: "Apple EarPods",
+      price: 2327,
+      rate: 4.5,
+   },
+]
+const wirelessHeadphones = [
+   {
+      img: "img/image_4.png",
+      title: "Apple AirPods",
+      price: 9527,
+      rate: 4.7,
+   },
+   {
+      img: "img/image_5.png",
+      title: "GERLAX GH-04",
+      price: 6527,
+      rate: 4.7,
+   },
+   {
+      img: "img/image_6.png",
+      title: "BOROFONE BO4",
+      price: 7527,
+      rate: 4.7,
+   },
+
+]
